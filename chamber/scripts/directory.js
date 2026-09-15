0a06bfeb84256ccaf5de23a24b00e695ef5762ec
lawrenetalkss <lawrene00@gmail.com>
2026-09-15 19:25:49 +0100

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -96,3 +96,8 @@ listViewBtn.addEventListener('click', () => setView('list'));
 
 // Initialize: fetch and render
 getMembers();
+<img src="images/${member.image}"
+     alt="${member.name} logo"
+     loading="lazy"
+     width="60"
+     height="60" />
